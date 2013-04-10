e79abd5e498706c12bc6da6e0a31a12d3e201c5b
marianogvarela@gmail.com <marianogvarela@gmail.com@5a5a0908-f5e5-8860-2943-7ad1662ee93a>
2013-04-10 21:34:27 +0000

    Almanaque zombie encaminado

---  ui-plantas-vs-zombies/TP1-PlantasVsZombies-UI/src/main/java/plantasVsZombies/Tablero.java
+++  ui-plantas-vs-zombies/TP1-PlantasVsZombies-UI/src/main/java/plantasVsZombies/Tablero.java
@@ -10,8 +10,15 @@ import plantaszombies.Jardin;
 @SuppressWarnings("serial")
 @Observable
 public class Tablero implements Serializable{
-	
-	private Jardin jardin;
+	private Jardin jardin = new Jardin (2,2);
+
+	public int getRecursos() {
+		return this.jardin.getRecursos();
+	}
+
+	public void setRecursos(int recursos) {
+		this.jardin.setRecursos(recursos);
+	}
 
 	public Jardin getJardin() {
 		return jardin;
